69be05f074114afde5d7d23da3ca52f785cc19b0
hiroshinakao <hiroshi.nakao@gmail.com>
2012-11-10 15:30:02 +0900

    $ rails new jenkins_sandbox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsSandbox::Application.config.secret_token = 'c251e2f8f23d39e7f30f9e088ff621631e3d9ceea52fe2315ffa7831f75c3b3620f07273322aa5a22db50029256ef7527442fb66bb61e449b22e49af88b6f5e4'
