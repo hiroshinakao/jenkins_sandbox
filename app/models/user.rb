02415fde11076f52d0cf2551f57a55ac3bcc5281
hiroshinakao <hiroshi.nakao@gmail.com>
2012-11-16 11:23:51 +0900

    姓名をくっつけてフルネームを返す User#full_name を追加。 カバレッジを見やすくするため、変な書き方をしています

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :profile
+
+  # 姓名をくっつけてフルネームを返します
+  def full_name
+    # カバレッジ結果を見やすくするため、ちょっと面倒な書き方をしています。
+    # しかも、フツーは苗字がないからって代理文字とか入れないですね…。
+
+    result = ""
+
+    if first_name
+      result << first_name
+    else
+      result << "Foo"
+    end
+
+    result << " "
+
+    if last_name
+      result << last_name
+    else
+      result << "Bar"
+    end
+
+    result
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
+#encoding: utf-8
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe :full_name do
+
+    context "姓名が揃っている場合" do
+      subject { User.new(first_name: "Dio", last_name: "Brando")}
+      its(:full_name) {should == "Dio Brando"}
+    end
+
+  end
 end
